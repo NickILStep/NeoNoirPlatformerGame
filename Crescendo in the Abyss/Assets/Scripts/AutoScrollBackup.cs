2e858440924047c2231a3fd273f967f1943c7fe7
nathandavis18 <nathandavis8@mail.weber.edu>
2024-02-22 14:38:32 -0700

    Player no longer goes offscreen

--- Crescendo in the Abyss/Assets/Scripts/AutoScrollBackup.cs
+++ Crescendo in the Abyss/Assets/Scripts/AutoScrollBackup.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 
 public class AutoScrollBackup : MonoBehaviour
 {
-    public float scrollSpeed = 2f;
+    private float scrollSpeed;
+    private float followScrollSpeed;
+    public Transform player;
     // Start is called before the first frame update
     void Start()
     {
+        scrollSpeed = 1.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.localPosition += Vector3.up * scrollSpeed * Time.deltaTime;
+        if(player.position.y > transform.position.y + 3)
+        {
+            followScrollSpeed = scrollSpeed + (player.position.y - transform.position.y);
+            transform.localPosition += Vector3.up * followScrollSpeed * Time.deltaTime;
+        }
     }
 }
